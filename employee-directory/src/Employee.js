dbe106c55ca566bf250671b50367fb5503667169
Adam Grossman <adgrossm@gmail.com>
2020-09-28 20:20:00 -0400

    added sorting of first and last name on click

--- employee-directory/src/Employee.js
+++ employee-directory/src/Employee.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import Title from "./components/Title/title";
 import employees from "./employee.json";
-// import Wrapper from "./components/Wrapper";
+import Wrapper from "./components/Wrapper/Wrapper";
 
 // need to create the movies.json file like Max did with id, title, year
 
@@ -11,37 +11,66 @@ class Employees extends React.Component {
     employeeList: employees,
     sortOrder: ''
   }
-     handleSort = () => {
-      console.log("test")
-   let newSort
-   if (this.state.sortOrder !== "des"){
-   newSort = this.state.employeeList.sort((a, b) => {
-      return a.departmentId - b.departmentId; 
-     } 
-   );
-}
-else {
-  newSort = this.state.employeeList.sort((a, b) => {
-    return b.departmentId - a.departmentId; 
-   } 
- );
-}
-console.log(newSort);
-const newSortOrder = this.state.sortOrder === "des"? "asc": "des"
-this.setState({employeeList: newSort, sortOrder: newSortOrder})
-}
-  render() {
-    return (
-      <>
-     <Title>Employee Directory </Title>
+  handleSort = (whatToSort) => {
+    console.log("test")
+    const newSort = this.state.employeeList.sort((a, b) => {
+      if (this.state.sortOrder !== "des") {
+        return a[whatToSort] - b[whatToSort];
+      } else {
+        return b[whatToSort] - a[whatToSort];
+      }
+    }
+    );
+    console.log(newSort);
+    const newSortOrder = this.state.sortOrder === "des" ? "asc" : "des"
+    this.setState({ employeeList: newSort, sortOrder: newSortOrder })
+  }
+
+  handleNameSort = (nameToSort) => {
+    const newSort = this.state.employeeList.sort((a, b) => {
+      var nameA = a[nameToSort].toUpperCase();
+      var nameB = b[nameToSort].toUpperCase();
+      if (this.state.sortOrder !== "des") {
+        if (nameA < nameB) {
+          return -1;
+        }
+        if (nameA > nameB) {
+          return 1;
+        }
+        // names must be equal
+        return 0;
+      } else {
+        if (nameA < nameB) {
+          return 1;
+        }
+        if (nameA > nameB) {
+          return -1;
+        }
+        // names must be equal
+        return 0;
+      }
+    }
+    );
+    console.log(newSort);
+    const newSortOrder = this.state.sortOrder === "des" ? "asc" : "des"
+    this.setState({ employeeList: newSort, sortOrder: newSortOrder })
+  };
+
+render() {
+  return (
+    <>
+
+      <Title>Employee Directory </Title>
+      <Wrapper>
+        <div><input type="text" /><button>Search</button></div>
         <table className="table table-dark">
           <thead>
             <tr>
-              <th scope="col" onClick={()=> this.handleSort()}>Employee ID#</th>
-              <th scope="col">First Name</th>
-              <th scope="col">Last Name</th>
-              {/* <th scope="col">Department</th> */}
-              <th scope="col" onClick={()=> this.handleSort()}>Department ID</th>
+              <th scope="col" onClick={() => this.handleSort("id")}>Employee ID#</th>
+              <th scope="col" onClick={()=> this.handleNameSort("firstName")}>First Name</th>
+              <th scope="col"onClick={()=> this.handleNameSort("lastName")}>Last Name</th>
+              <th scope="col">Department</th>
+              <th scope="col" onClick={() => this.handleSort("departmentId")}>Department ID</th>
             </tr>
           </thead>
           <tbody>
@@ -50,14 +79,16 @@ this.setState({employeeList: newSort, sortOrder: newSortOrder})
                 <th scope="row">{employees.id}</th>
                 <td>{employees.firstName}</td>
                 <td>{employees.lastName}</td>
+                <td>{employees.department}</td>
                 <td>{employees.departmentId}</td>
               </tr>
             ))}
           </tbody>
-        </table> 
-      </> 
-    );
-  };
+        </table>
+      </Wrapper>
+    </>
+  );
+};
 }
 
 export default Employees
